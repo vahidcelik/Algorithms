8f80450633aef5e479f46215028a6ede3c4df461
vahid.celik <vahidcelik2@gmail.com>
2018-08-09 23:44:39 +0300

    It is the first commit

---
+++ HackerRank_CompareTheTriplets/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HackerRank_CompareTheTriplets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens_a0 = Console.ReadLine().Split(' ');
+            int a0 = Convert.ToInt32(tokens_a0[0]);
+            int a1 = Convert.ToInt32(tokens_a0[1]);
+            int a2 = Convert.ToInt32(tokens_a0[2]);
+            string[] tokens_b0 = Console.ReadLine().Split(' ');
+            int b0 = Convert.ToInt32(tokens_b0[0]);
+            int b1 = Convert.ToInt32(tokens_b0[1]);
+            int b2 = Convert.ToInt32(tokens_b0[2]);
+            int[] result = solve(a0, a1, a2, b0, b1, b2);
+            Console.WriteLine(String.Join(" ", result));
+            int[] a = { 0, int.MaxValue };
+        }
+
+        static int[] solve(int a0, int a1, int a2, int b0, int b1, int b2)
+        {
+            int[] points = { 0, 0 };
+
+            compare(a0, b0, points);
+            compare(a1, b1, points);
+            compare(a2, b2, points);
+
+            return points;
+        }
+
+        static void compare(int ai, int bi, int[] points)
+        {
+            if (ai > bi)
+                points[0]++;
+            else if (bi > ai)
+                points[1]++;
+        }
+    }
+}
---
+++ HackerRank_MinMaxSum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HackerRank_MinMaxSum
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            long[] arr = Array.ConvertAll(arr_temp, Int64.Parse);
+            Array.Sort(arr);
+            long[] result = { arr[0] + arr[1] + arr[2] + arr[3], arr[1] + arr[2] + arr[3] + arr[4] }; //min, max
+            Console.WriteLine(String.Join(" ", result));
+        }
+
+
+        
+    }
+}
